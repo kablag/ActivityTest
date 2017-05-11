49d583677416463474fad357a4f9fb264381a6a0
kablag <k.blag@yandex.ru>
2017-05-11 16:26:21 +0300

    SE start add

--- server.R
+++ server.R
@@ -26,6 +26,17 @@ shinyServer(function(input, output, session) {
       filter(target == input$targetSlct)
   })
 
+  output$quantitiesSlctUI <- renderUI({
+    req(description())
+    quantities <- unique(description()$quantity)
+    quantities <- quantities[!is.na(quantities)]
+    selectInput("quantitiesSlct",
+                "Use Input Units",
+                quantities,
+                quantities,
+                multiple = TRUE)
+  })
+
   fData <- reactive({
     req(description())
     rdmlFile()$GetFData(description())
@@ -41,23 +52,34 @@ shinyServer(function(input, output, session) {
   slopeResultsTbl <- reactive({
     req(mlist())
     isolate({
-      effs <- map(mlist(), ~ efficiency(., plot = FALSE))
+      effs <- map(mlist(), ~
+                    tryCatch(
+                      efficiency(., plot = FALSE),
+                      error = function(e) list(cpD1 = NA,
+                                               cpD2 = NA)
+                    )
+      )
       description() %>%
         group_by(fdata.name) %>%
         mutate(cpD1 = effs[[fdata.name]][["cpD1"]],
                cpD2 = effs[[fdata.name]][["cpD2"]],
-               cpD1fluo = predict(mlist()[[fdata.name]],
-                                  newdata = data.frame(Cycles = cpD1))[1,1],
-               cpD2fluo = predict(mlist()[[fdata.name]],
-                                  newdata = data.frame(Cycles = cpD2))[1,1],
-               slope = (cpD1fluo - cpD2fluo)/(cpD1 - cpD2))
+               cpD1fluo = tryCatch(
+                 predict(mlist()[[fdata.name]],
+                         newdata = data.frame(Cycles = cpD1))[1,1],
+                 error = function(e) NA),
+               cpD2fluo = tryCatch(
+                 predict(mlist()[[fdata.name]],
+                         newdata = data.frame(Cycles = cpD2))[1,1],
+                 error = function(e) NA),
+               slope = round((cpD1fluo - cpD2fluo)/(cpD1 - cpD2), 2))
     })
   })
 
   activityModel <- reactive({
-    req(slopeResultsTbl())
+    req(slopeResultsTbl(), input$quantitiesSlct)
     stdTbl <- slopeResultsTbl() %>%
-      filter(sample.type == "std")
+      filter(sample.type == "std",
+             quantity %in% input$quantitiesSlct)
     Slope <- stdTbl$slope
     Quantity <- stdTbl$quantity
     # lm(log(Quantity) ~ Slope)
@@ -74,41 +96,79 @@ shinyServer(function(input, output, session) {
     req(activityModel())
     isolate({
       slopeResultsTbl() %>%
-        ungroup() %>%
         mutate(
           # punits = exp(predict(activityModel(), newdata = data.frame(Slope = slope)))
-          punits = predict(activityModel(), newdata = data.frame(Slope = slope))) %>%
+          prediction = list(predict(activityModel(), newdata = data.frame(Slope = slope),
+                                    interval = c("prediction"))),
+          punits = unlist(prediction)[1] %>% round(2),
+          punitsSE = unlist(prediction)[2] %>% round(2)) %>%
         group_by(sample) %>%
         mutate(
-          meanPunits = mean(punits)
-          ) %>%
+          meanPunits = mean(punits) %>%
+            round(2),
+          meanPunitsSE = mean(punitsSE) %>%
+            round(2)) %>%
         ungroup()
     })
   })
 
-  output$resultsTable <- renderTable({
+  output$resultsTable <- DT::renderDataTable({
     req(punitsResultsTbl())
     punitsResultsTbl() %>%
+      mutate(stockUnits = meanPunits * input$stockFactor) %>%
       select(Well = position,
              Name = sample,
              Type = sample.type,
              "Input Units" = quantity,
              "RFU/Cycle" = slope,
-             "Predicted Units" = punits,
-             "Predicted Units Mean" = meanPunits
-             )
+             "Pred. Units" = punits,
+             "Pred. Units Mean" = meanPunits,
+             "Pred. Units Mean SE" = meanPunitsSE,
+             "Stock Units" = stockUnits
+             ) %>%
+      DT::datatable(options = list(paging = FALSE),
+                    selection = "single")
+  })
+
+  output$curvePlot <- renderPlot({
+    req(input$resultsTable_rows_selected)
+    selected <- punitsResultsTbl() %>%
+      filter(position ==
+               as.character(punitsResultsTbl()
+                                      [input$resultsTable_rows_selected, "position"])) %>%
+      as.data.frame()
+    curveName <- selected[1, "fdata.name"]
+    prediction <- predict(mlist()[[curveName]])$Prediction
+    plot(x = 1:length(prediction),
+         y = prediction,
+         type = "l",
+         xlab = "Cycle", ylab = "RFU", col = "grey", lwd = 2)
+    lines(x = c(selected[1, "cpD2"],
+            selected[1, "cpD1"]),
+          y = c(selected[1, "cpD2fluo"],
+            selected[1, "cpD1fluo"]),
+          col = "red", lwd = 2)
+    points(x = fData()$cyc, y = fData()[[curveName]])
+    # (c(cpD2, cpD1), c(cpD2fluo, cpD1fluo), col = "red", size = 5)
   })
 
   output$calibrationPlot <- renderPlot({
     req(punitsResultsTbl())
-    stdTbl <- slopeResultsTbl() %>%
+    stdTbl <- punitsResultsTbl() %>%
       filter(sample.type == "std")
     unknTbl <- punitsResultsTbl() %>%
       filter(sample.type == "unkn")
-    plot(stdTbl$quantity, stdTbl$slope, xlab = "Units", ylab = "RFU/Cycle")
-    # lines(exp(predict(activityModel())), stdTbl$slope, lty = 3, col = "red", size = 5)
-    lines(predict(activityModel()), stdTbl$slope, lty = 3, col = "red", size = 5)
-    points(unknTbl$punits, unknTbl$slope, col = "blue", size = 5)
+    plot(stdTbl$quantity, stdTbl$slope,
+         xlab = "Units", ylab = "RFU/Cycle")
+    # lines(exp(predict(activityModel())), stdTbl$slope, lty = 3, col = "red")
+    lines(stdTbl$punits, stdTbl$slope,
+          lty = 3, col = "red", lwd = 2)
+    points(unknTbl$punits, unknTbl$slope,
+           col = "blue")
+    # ggplot() +
+    #   geom_point(data = unknTbl, aes(x=quantity, y=slope), color = "grey") +
+    #   geom_point(data = unknTbl, aes(x=punits, y=slope, color = sample)) +
+    #   theme_bw()
   })
 
 
--- ui.R
+++ ui.R
@@ -6,12 +6,22 @@ shinyUI(fluidPage(
   mainPanel(
     fileInput("rdmlFileInput",
               HTML("Upload <b>.RDML</b> file:")),
-    uiOutput("targetSlctUI"),
-    htmlOutput("activityModelSummary"),
+    fluidRow(
+      column(4,
+             uiOutput("targetSlctUI")),
+      column(4,
+             uiOutput("quantitiesSlctUI")),
+      column(4,
+             numericInput("stockFactor", "Stock Factor", 25, 1, step = 1))),
     # tags$ul(
     #   htmlOutput("activityModelSummary", container = tags$li, class = "custom-li-output")
     # ),
-    plotOutput("calibrationPlot"),
-    tableOutput("resultsTable")
+    fluidRow(
+      column(6,
+             plotOutput("calibrationPlot")),
+      column(6,
+             plotOutput("curvePlot"))),
+    htmlOutput("activityModelSummary"),
+    DT::dataTableOutput("resultsTable")
   )
 ))
