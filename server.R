90200c55840ff16efaede14bd9c48661eba2d9ef
kablag <k.blag@yandex.ru>
2020-07-29 14:41:47 +0300

    multiple curves view

--- server.R
+++ server.R
@@ -23,6 +23,7 @@ shinyServer(function(input, output, session) {
 
   rdmlFile <- reactive({
     req(input$rdmlFileInput)
+    cat("loading file\n")
     newpath <- paste(input$rdmlFileInput$datapath[1],
                      file_ext(input$rdmlFileInput$name[1]),
                      sep = ".")
@@ -33,6 +34,7 @@ shinyServer(function(input, output, session) {
 
   output$targetSlctUI <- renderUI({
     req(rdmlFile())
+    cat("creating targetSlct\n")
     selectInput("targetSlct",
                 "Target",
                 names(rdmlFile()$target))
@@ -41,7 +43,9 @@ shinyServer(function(input, output, session) {
   description <- reactive({
     cat("gen description\n")
     req(input$targetSlct)
-    rdmlFile()$AsTable(quantity = sample[[react$sample$id]]$quantity$value) %>%
+    rdmlFile()$AsTable(
+      quantity =
+        sample[[react$sample$id]]$quantity$value) %>%
       filter(target == input$targetSlct,
              adp == TRUE) %>%
       mutate(logQuantity = log(quantity))
@@ -49,6 +53,7 @@ shinyServer(function(input, output, session) {
 
   output$quantitiesSlctUI <- renderUI({
     req(description())
+    cat("creating quantitiesSlct\n")
     quantities <- unique(description()$quantity)
     quantities <- sort(quantities[!is.na(quantities)])
     checkboxGroupButtons(
@@ -61,22 +66,49 @@ shinyServer(function(input, output, session) {
     )
   })
 
-  fData <- reactive({
+  output$useWellsUI <- renderUI({
     req(description())
-    as.data.frame(rdmlFile()$GetFData(description()))
+    cat("creating useWellsSlct\n")
+    wells <- unique(description()$position)
+    selectInput("useWellsSlct", "Use Wells",
+                wells, wells, multiple = TRUE)
   })
 
+  fData <- reactive({
+    req(description(), input$useWellsSlct)
+    cat("getting fdata\n")
+    as.data.frame(rdmlFile()$GetFData(description()[position %in% input$useWellsSlct]))
+  })
+
+  output$individualSlopeRegionsUI <- renderUI({
+    req(description())
+    cat("creating individualSlopeRegionsUI")
+    lapply(unique(description()$sample), function(sname)
+      sliderInput(paste0("slopeRegion_", sname),
+                  paste0("Slope Region: ", sname),
+                  min = 1, max = 50, c(10, 20),
+                  step = 1))
+  })
 
   mlist <- reactive({
     req(fData())
     cat("calc mlist\n")
     ml <- lapply(colnames(fData())[-1],
                   function(cname) {
+                    slopeRegion <- {
+                      if (input$useGlobalSlope)
+                        input$slopeRegion
+                      else {
+                        sname <- description()[fdata.name == cname, c(sample)]
+                        input[[paste0("slopeRegion_", sname)]]
+                      }
+                    }
+
                     fpoints <-
                       data.frame(
-                        cyc = input$slopeRegion[1]:input$slopeRegion[2],
+                        cyc = slopeRegion[1]:slopeRegion[2],
                         fluor = fData()[
-                          input$slopeRegion[1]:input$slopeRegion[2],
+                          slopeRegion[1]:slopeRegion[2],
                           cname])
                     tryCatch(
                       lm(fluor ~ cyc, fpoints),
@@ -92,7 +124,7 @@ shinyServer(function(input, output, session) {
     req(input$endPointCycle)
     cat("calc slopeResultsTbl\n")
     isolate({
-      description() %>%
+      description()[position %in% input$useWellsSlct] %>%
         group_by(fdata.name) %>%
         mutate(
           slope = mlist()[[fdata.name]]$coefficients[2],
@@ -243,47 +275,65 @@ shinyServer(function(input, output, session) {
         options = list(paging = FALSE,
                        dom = 'Bfrtip',
                        buttons = c('copy', 'csv', 'excel')),
-        selection = "single")
+        selection = "multiple")
   })
 
   output$curvePlot <- renderPlotly({
     req(input$resultsTable_rows_selected)
     cat("drawing curvePlot\n")
-    selected <- punitsResultsTbl() %>%
-      filter(position ==
-               as.character(punitsResultsTbl()
-                            [input$resultsTable_rows_selected, "position"])) %>%
-      as.data.frame()
-    curveName <- selected[1, "fdata.name"]
-    prediction <- predict(mlist()[[curveName]])
-    fdt <- data.frame(cyc = fData()[["cyc"]],
-                      fluor = fData()[[curveName]])
+    # selected <- punitsResultsTbl() %>%
+    #   filter(position %in%
+    #            punitsResultsTbl()[input$resultsTable_rows_selected, "position"]) %>%
+    #   as.data.frame()
+    selected <- punitsResultsTbl()[input$resultsTable_rows_selected, ]
+    curveNames <- selected[["fdata.name"]]
 
-    slopeLine <- data.frame(cyc = input$slopeRegion,
-                            fluor = c(prediction[1], tail(prediction, 1)))
+    fdt <- lapply(curveNames, function(curveName) {
+      data.frame(cyc = fData()[["cyc"]],
+                 fluor = fData()[[curveName]],
+                 sname = curveName)
+    })
+    fdt <- bind_rows(fdt)
+
+    slopeLine <- lapply(curveNames, function(curveName) {
+      slopeRegion <- {
+        if (input$useGlobalSlope)
+          input$slopeRegion
+        else {
+          sname <- description()[fdata.name == curveName, c(sample)]
+          input[[paste0("slopeRegion_", sname)]]
+        }
+      }
+      prediction <- predict(mlist()[[curveName]])
+      data.frame(cyc = slopeRegion,
+                 fluor = c(prediction[1], tail(prediction, 1)),
+                 sname = curveName)
+
+    })
+    slopeLine <- bind_rows(slopeLine)
+
     ggplot(fdt) +
-      geom_line(aes(cyc, fluor)) +
-      geom_line(data = slopeLine, aes(x = cyc, y = fluor), color = "red") +
-      theme_bw()
+      geom_line(aes(x = cyc, y = fluor, color = sname)) +
+      geom_line(data = slopeLine, aes(x = cyc, y = fluor, group = sname), color = "black") +
+      theme_bw() +
+      theme(legend.title=element_blank())
   })
 
   output$meltPlot <- renderPlotly({
     req(input$resultsTable_rows_selected)
     cat("drawing meltPlot\n")
-    selected <- punitsResultsTbl() %>%
-      filter(position ==
-               as.character(punitsResultsTbl()
-                            [input$resultsTable_rows_selected, "position"])) %>%
-      as.data.frame()
-    # curveName <- selected[1, "fdata.name"]
+    selected <- punitsResultsTbl()[input$resultsTable_rows_selected, ]
     meltData <- tryCatch(
       rdmlFile()$GetFData(selected, dp.type = "mdp", long.table = TRUE),
       error = function(e) NULL)
     req(meltData)
-    meltData$diffFluor <- diff(meltData$fluor) * -1
-    ggplot(meltData, aes(tmp, diffFluor)) +
+    meltData <- meltData %>%
+      group_by(fdata.name) %>%
+      mutate(diffFluor := c(0, diff(fluor) * -1))
+    ggplot(meltData, aes(x = tmp, y = diffFluor, color = fdata.name)) +
       geom_line() +
-      theme_bw()
+      theme_bw() +
+      theme(legend.position = "none")
   })
 
   output$calibrationPlot <- renderPlotly({
